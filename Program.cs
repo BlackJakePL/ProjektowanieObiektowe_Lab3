1ac6210ed4bf4e46b1ca7b9359b1761cc9fd9096
BlackJakePL <47294105+BlackJakePL@users.noreply.github.com>
2021-04-18 12:25:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using Lab3;
+
+namespace Lab3
+{
+    public class Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public virtual void Draw()
+        {
+
+            Console.WriteLine("Rysuje ksztalt");
+
+        }
+    }
+
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+
+            Console.WriteLine("Rysuje prostokat");
+
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+
+            Console.WriteLine("Rysuje Trójkat");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+
+            Console.WriteLine("Rysuje Trójkat");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> list = new List<Shape>();
+
+            list.Add(new Shape()
+            {
+                X = 3,
+                Y = 4,
+                Height = 2,
+                Width = 6
+
+            }
+            );
+
+            list.Add(new Rectangle()
+            { 
+                X=2,
+                Y=5,
+                Height=3,
+                Width=3
+            }
+            );
+
+            list.Add(new Triangle()
+            {
+                X = 2,
+                Y = 5,
+                Height = 3,
+                Width = 3
+            }
+            );
+
+            list.Add(new Circle()
+            {
+                X = 2,
+                Y = 5,
+                Height = 3,
+                Width = 3
+            }
+            );
+
+
+
+            foreach (var item in list)
+            {
+                item.Draw();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
